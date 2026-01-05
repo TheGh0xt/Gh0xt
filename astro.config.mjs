0d3605549b1ac7367e030a219d18bc8452de00ad
TheGh0xt <adetoyeanointing15@gmail.com>
2026-01-05 05:26:24 +0100

    revert: switch back to Vercel SSR for instant article updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,18 +6,23 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
 import mdx from "@astrojs/mdx";
+import vercel from "@astrojs/vercel/serverless";
 
 
 import sanity from "@sanity/astro";
 
 export default defineConfig({
-  site: 'https://TheGh0xt.github.io',
-  base: '/',
-  output: 'static',
+  output: 'server',
   integrations: [tailwind(), react(), mdx(), sanity({
     projectId: 'kciy3tvs',
     dataset: 'production',
 
     useCdn: false,
   })],
+
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
